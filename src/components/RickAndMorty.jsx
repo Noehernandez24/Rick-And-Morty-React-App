4e5499c4b09c511c3d58f585608e2b3e9ae243ec
noehernandez24 <evilld94@gmail.com>
2023-01-06 11:44:00 -0600

    CHANGE: TimeOut

--- src/components/RickAndMorty.jsx
+++ src/components/RickAndMorty.jsx
@@ -35,7 +35,7 @@ const RickAndMorty = () => {
                 setDimension(res.data)
                 setTimeout(() => {
                     setApiLoader(false)
-                }, 300);
+                }, 500);
             })
         } 
     }
